1c642c4554d370425f176a86b860771d1994e849
LiWen LI <764262419@qq.com>
2025-06-19 10:56:33 +0800

    Update PICK_MASK.py

--- Train/PICK_MASK.py
+++ Train/PICK_MASK.py
@@ -10,9 +10,10 @@ AUTO = tf.data.experimental.AUTOTUNE
 from scipy.spatial.distance import pdist,squareform
 
 class Data_Pre:
-  def __init__(self, total_std_path, total_var_path, maxatom=50,  d_cut = 5,  len_dictionary = 5,  Tranining = False):
+  def __init__(self, total_std_path, total_var_path, picknoise = 2, maxatom=50,  d_cut = 5,  len_dictionary = 5,  Tranining = False):
     self.total_path = total_std_path
     self.total_var_path = total_var_path
+    self.picknoise = picknoise
     self.maxatom = maxatom
     self.d_cut = d_cut
     self.len_dictionary = len_dictionary
@@ -33,14 +34,18 @@ class Data_Pre:
       sorted_indices = np.argsort(row[small_indices])[:max_small_index]
       indices.append(small_indices[sorted_indices])
     return indices
+    
+  def pick_not_replet (self, path, ni):
+    selected_paths = random.sample(list(path), ni)
+    return selected_paths
   
-  def read_npy_file(self, filename1, filename2, filename_list):
+  def read_npy_file(self, filename1, filename2, filename_list, ni):
     filename1 = str(filename1)
     filename2 = str(filename2)
+    select_ni = None
     if self.Tranining == True:
-      for i in range(len(filename_list)):
-        filename_list[i] = str(filename_list[i])
-        tf.print(filename_list[i])
+      select_ni = pick_not_replet(filename_list,ni)
+      tf.print(filename_list[i])
     tf.print(filename1)
     tf.print(filename2)
     label = int(filename.split('d/')[1].split('.')[0])  #label
