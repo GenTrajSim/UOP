0d9c7bd07ac9f33981044749b1b8e05a6691f930
LiWen LI <764262419@qq.com>
2025-06-20 17:18:08 +0800

    Update model.py

--- Trains/model.py
+++ Trains/model.py
@@ -1,6 +1,205 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import self_attention 
-import encoder
-import tool
+import self_attention as sat
+import encoder 
+import UOPtool as uopt
+
+class Gen3Dmol_Classify(tf.keras.layers.Layer):
+    def __init__(
+        self, 
+        encoder_layers = 15,
+        encoder_embed_dim = 512,
+        encoder_ffn_embed_dim = 2048,
+        encoder_attention_heads = 32,
+        Natom = 50,
+        iterT = 1000,
+        dropout = 0.1,
+        emb_dropout = 0.1,
+        attention_dropout = 0.1,
+        activation_dropout = 0.0,
+        pooler_dropout = 0.0,
+        max_seq_len = 512,
+        #activation_fn = "rel
+        #pooler_activation_fn = "tanh",
+        post_ln = False,
+        masked_token_loss = -1.0,
+        masked_token_pred = 1.0,
+        masked_coord_loss = -1.0,
+        masked_dist_loss = -1.0,
+        x_norm_loss = -1.0,
+        delta_pair_repr_norm_loss = -1.0,
+        num_classes = 3,
+        crytal_class = 10,
+        dictionary = None
+    ):
+        super(Gen3Dmol_Classify, self).__init__()
+        #self.padding_idx = dictionary['MASK']
+        self.padding_idx = 0 
+        ##
+        self.encoder_layers = encoder_layers
+        self.encoder_embed_dim = encoder_embed_dim
+        self.encoder_ffn_embed_dim = encoder_ffn_embed_dim
+        self.encoder_attention_heads = encoder_attention_heads
+        self.Natom = Natom
+        self.iterT = iterT
+        self.dropout = dropout
+        self.emb_dropout = emb_dropout
+        self.attention_dropout = attention_dropout
+        self.activation_dropout = activation_dropout
+        self.pooler_dropout =  pooler_dropout
+        self.max_seq_len = max_seq_len
+        #activation_fn = "rel
+        #pooler_activation_fn = "tanh",
+        self.post_ln = post_ln
+        self.masked_token_loss = masked_token_loss
+        self.masked_token_pred = masked_token_pred
+        self.masked_coord_loss = masked_coord_loss
+        self.masked_dist_loss = masked_dist_loss
+        self.x_norm_loss =x_norm_loss                        ############
+        self.delta_pair_repr_norm_loss = delta_pair_repr_norm_loss#######
+        self.num_classes = num_classes
+        self.crytal_class = crytal_class
+        ##
+        self.embed_tokens = tf.keras.layers.Embedding(
+            len(dictionary), self.encoder_embed_dim, mask_zero=True
+        )
+        self.encoder = encoder.TransformerEncoderWithPair(
+            encoder_layers = self.encoder_layers, 
+            embed_dim = self.encoder_embed_dim, 
+            ffn_embed_dim = self.encoder_ffn_embed_dim, 
+            attention_heads = self.encoder_attention_heads, 
+            emb_dropout = self.emb_dropout, 
+            dropout = self.dropout, 
+            attention_dropout = self.attention_dropout, 
+            activation_dropout = self.activation_dropout, 
+            max_seq_len = self.max_seq_len, #???
+            # activation_fn = "gel
+            post_ln = False, 
+            no_final_head_layer_norm = self.delta_pair_repr_norm_loss
+        )
+        if self.masked_token_loss > 0:
+            self.lm_head = uopt.MaskLMHead(
+                embed_dim = self.encoder_embed_dim,
+                output_dim = self.crytal_class,
+                weight=None
+            )
+        K = 128
+        n_edge_type = len(dictionary) * len(dictionary)
+        self.gbf_proj = uopt.NonLinearHead(
+            K+1, self.encoder_attention_heads
+        )
+        self.gbf = uopt.GaussianLayer(K, n_edge_type)
+        self.ebb = uopt.embeding_PT_iter_P0ro1(self.iterT, self.Natom) #######################################################
+        if self.masked_coord_loss > 0:
+            self.pair2coord_proj  = uopt.NonLinearHead(
+                self.encoder_attention_heads, 1
+            )
+        if self.masked_dist_loss > 0:
+            self.dist_head = uopt.DistanceHead(
+                self.encoder_attention_heads
+            )
+        self.classification_heads = uopt.ClassificationHead(input_dim = self.encoder_embed_dim,
+                                                       inner_dim = self.encoder_embed_dim,
+                                                       num_classes = self.num_classes, 
+                                                       #activation_fn, 
+                                                       pooler_dropout = self.pooler_dropout)
+    # classmmethod
+    # def build_model(cls, args, task): 
+    # return cls(args, task.dictionary)
+    def call(
+        self,
+        src_tokens,
+        src_distance,
+        src_coord,
+        src_edge_type,
+        press, temp, iter_T, Predict01,
+        encoder_masked_tokens=None,   # always None
+        Not_only_features=False,
+        training = False
+        #classification_head_name=True
+    ):
+        #if not classification_head_name:
+        #    features_only = True
+        padding_mask = tf.equal(src_tokens, self.padding_idx)
+        #if not tf.reduce_any(padding_mask):
+        #    padding_mask = None
+        x = self.embed_tokens(src_tokens)
+        embedding_bias = self.ebb(press, temp, iter_T, Predict01) ######################################################
+        def get_dist_features(dist, et):
+            n_node = dist.shape[-1]
+            gbf_feature = self.gbf(dist, et)
+            gbf_feature = tf.concat([gbf_feature,embedding_bias], axis=-1)
+            gbf_result = self.gbf_proj(gbf_feature)
+            graph_attn_bias = gbf_result
+            graph_attn_bias = tf.transpose(graph_attn_bias, perm=[0,3,1,2])
+            graph_attn_bias = tf.reshape(graph_attn_bias, (-1, n_node, n_node))
+            return graph_attn_bias
+        graph_attn_bias = get_dist_features(src_distance, src_edge_type)
+        (
+            encoder_rep,
+            encoder_pair_rep,
+            delta_encoder_pair_rep,
+            x_norm,
+            delta_encoder_pair_rep_norm,
+        ) = self.encoder(x, padding_mask=padding_mask, attn_mask=graph_attn_bias,training = training)
+        #encoder_pair_rep[encoder_pair_rep == float("-inf")] = 0
+        encoder_pair_rep = tf.where(tf.math.is_finite(encoder_pair_rep), encoder_pair_rep, tf.cast(tf.zeros_like(encoder_pair_rep),dtype=tf.float32))
+        encoder_distance = None
+        encoder_coord = None
+        #if not features_only:
+        if self.masked_token_loss > 0:
+            logits = self.lm_head(encoder_rep, encoder_masked_tokens)
+        if self.masked_coord_loss > 0:
+            coords_emb = src_coord
+            if padding_mask is not None:
+                atom_num = tf.reshape((tf.reduce_sum( 1 - tf.cast(padding_mask, x.dtype) , axis=1) -1),
+                                      (-1, 1, 1, 1))
+            else:
+                atom_num = tf.shape(src_coord, 1) - 1 ###?????check!!!!!!!!!!!!!!!!!!!!!!!!!!!!(-1)?????
+            delta_pos = tf.expand_dims(coords_emb, axis=1) - tf.expand_dims(coords_emb, axis=2)
+            attn_probs = self.pair2coord_proj(delta_encoder_pair_rep)
+            coord_update = delta_pos / atom_num * attn_probs
+            coord_update = tf.reduce_sum(coord_update, axis=2)
+            encoder_coord = coords_emb + coord_update   ###### coord_update noise output
+        if self.masked_dist_loss > 0:
+            encoder_distance = self.dist_head(encoder_pair_rep)
+        if Not_only_features:
+            logits_h = self.classification_heads(encoder_rep)
+            return (
+                logits,   ###token type
+                logits_h,  ##crystal type
+                encoder_distance,
+                encoder_coord,
+                x_norm,
+                delta_encoder_pair_rep_norm
+                )
+        else:
+            return (
+                logits,
+                encoder_distance,
+                encoder_coord,
+                x_norm,
+                delta_encoder_pair_rep_norm
+            )
+
+
+'''
+test:
+
+test_layer = Gen3Dmol_Classify(
+    masked_token_loss = 1.0,
+    masked_token_pred = 1.0,
+    masked_coord_loss = 1.0,
+    masked_dist_loss = 1.0,
+    x_norm_loss = 1.0,
+    delta_pair_repr_norm_loss = 1.0,
+    dictionary=dictionary)
+
+y = test_layer(token,
+               distance,
+               coord,
+               edge_type,
+               encoder_masked_tokens=None,   # always None
+               Not_only_features=True)
+'''
