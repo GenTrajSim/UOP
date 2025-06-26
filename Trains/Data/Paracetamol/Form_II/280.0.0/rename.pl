f3f21c3be3db88de5d50a273fe2563beac6fb418
liwen <liwenli.sim@gmail.com>
2025-06-26 11:33:00 +0800

    update test_dataset

---
+++ Trains/Data/Paracetamol/Form_II/280.0.0/rename.pl
@@ -0,0 +1,6 @@
+for (my $i = 1; $i<=100; $i++) {
+	my $name1 = 'dump.'.($i*1000).'.xyz';
+	my $name2 = '2.280.0.'.($i*1000).'.Paracetamol.xyz';
+	print "mv $name1 $name2\n";
+	system("mv $name1 $name2");
+}
